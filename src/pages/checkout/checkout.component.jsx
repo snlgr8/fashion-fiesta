3b6b7743eac45388a562eb4e175b8a731912dca7
Sonal Chaplot <snlgr8@gmail.com>
2020-10-18 15:47:38 +0530

    correcting file name

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-import StripeCheckoutButton from '../../components/stripe-button/stripr-button.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 import {
   selectCartItems,
   selectCartTotal,
